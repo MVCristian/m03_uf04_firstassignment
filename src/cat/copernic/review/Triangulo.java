641740e151b8978f579db253fbba233e1ef459e2
Cristian_21 <mvcristian13@gmail.com>
2020-10-13 20:45:05 +0200

    Finalizado

--- src/cat/copernic/review/Triangulo.java
+++ src/cat/copernic/review/Triangulo.java
@@ -14,21 +14,49 @@ public class Triangulo {
     // propiedades
     private double base;
     private double altura;
+
+    public Triangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
     
     /* Calcula el área de este triangulo */
     public double area() {
-        return 0.0;
+        return base*altura/2;
     }
     
     /* Calcula el perímetro de este triangulo conocida su base y altura */
-    public double perimetro() {
-        // Math.sqrt(345.9);
-        // Math.pow(n, 2.0);
-        return 0.0;
+     private double perimetro() {
+        double hipotenusa;
+        hipotenusa = Math.sqrt(((base/2)*(base/2)) + ((altura)*(altura)));
+        return hipotenusa*2+base;
+     }
+    
+
+       
+    public static void main(String[] args) {
+        Triangulo c1 = new Triangulo(19.0, 17.0);
+        System.out.println(c1);
+        System.out.println(c1.area());
+        System.out.println(c1.perimetro());
     }
     
-//    private double calcHipotenusa() {
-//        
-//    }
+
     
 }
