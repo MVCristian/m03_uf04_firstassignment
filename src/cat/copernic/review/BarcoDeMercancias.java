28e72c30512d528470744014e25bad444e43ea65
Cristian_21 <mvcristian13@gmail.com>
2020-10-19 21:11:51 +0200

    Barco de Mercancias

---
+++ src/cat/copernic/review/BarcoDeMercancias.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.copernic.review;
+
+/**
+ *
+ * @author HP
+ */
+public abstract class BarcoDeMercancias extends Barco {
+    
+    private int capacidadContenedores;
+
+    public BarcoDeMercancias(Motor motor, double eslora, double manga, int nudos, int peso, int capacidadContenedores) {
+        //llama al constructor de la superclase
+        super(motor, eslora, manga, nudos, peso);
+        this.capacidadContenedores = capacidadContenedores;
+        this.nombre = "Mi yate";
+    }
+    @Override
+    public void arranca(){
+        System.out.println("Arrancando barco de guerra");
+    }
+    
+    @Override
+    public void atraca(){
+        System.out.println("Atracando barco de guerra");
+    }
+
+    
+    
+    
+    
+    
+}
